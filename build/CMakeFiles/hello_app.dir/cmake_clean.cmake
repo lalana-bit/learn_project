ef80ff191619f9f7aa38507b2de8ef1c2eebf3ac
koharu <3262447331@qq.com>
2026-04-15 20:08:04 +0800

    init

---
+++ build/CMakeFiles/hello_app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_app.dir/hello.c.obj"
+  "CMakeFiles/hello_app.dir/hello.c.obj.d"
+  "CMakeFiles/hello_app.dir/main.c.obj"
+  "CMakeFiles/hello_app.dir/main.c.obj.d"
+  "hello_app.exe"
+  "hello_app.exe.manifest"
+  "hello_app.pdb"
+  "libhello_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/hello_app.dir/main.c.obj.d
@@ -0,0 +1,14 @@
+CMakeFiles/hello_app.dir/main.c.obj: \
+ C:\Users\lalana\Desktop\C_test\main.c \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/stdio.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/corecrt_stdio_config.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/corecrt.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/_mingw.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/_mingw_mac.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/_mingw_secapi.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/vadefs.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/_mingw_off_t.h \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/swprintf.inl \
+ D:/MinGW/i686-15.2.0-release-mcf-dwarf-ucrt-rt_v13-rev1/mingw32/i686-w64-mingw32/include/sec_api/stdio_s.h \
+ C:\Users\lalana\Desktop\C_test\hello.h
